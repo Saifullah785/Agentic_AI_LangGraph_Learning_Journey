26121284a3a9a1c85e52fef92295823455d7b985
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-13 19:06:29 +0500

    created using vs code
    while practicing the code in the lecture LangGraph_+_SQLite_Chatbot_with_Database_Integration

--- LangGraph_+_SQLite_Chatbot_with_Database_Integration/streamlit_frontend_database.py
+++ LangGraph_+_SQLite_Chatbot_with_Database_Integration/streamlit_frontend_database.py
@@ -40,3 +40,29 @@ if 'chat_threads' not in st.session_state:
 
 
 add_thread(st.session_state['thread_id'])
+
+
+# **************************************** Sidebar UI *********************************
+
+st.sidebar.title('LangGraph Chatbot')
+
+if st.sidebar.button('Reset Chat'):
+    reset_chat()
+
+st.sidebar.header('My Conversations')
+
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_conversation(thread_id)
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = 'user'
+            else:
+                role = 'assistant'
+            temp_messages.append({'role': role, 'content': msg.content})
+        st.session_state['messages_history'] = temp_messages
+
