a85d27fae2d7be5de28f177bf97bc062b3141d14
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-22 19:36:44 +0500

    created using vs code
    while practicing the code in the lecture of Langsmith_masterclass_Practice_Code

---
+++ Langsmith_masterclass_Practice_Code/3_rag_v1.py
@@ -0,0 +1,12 @@
+# pip install -u langchain langchain-openai langchain-community faiss-cpu pypdf python-dotenv
+
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+
