122553da194f6ca8c4c0f033e386be0dad4de918
Saif Ullah <muhammadalisaif785@gmail.com>
2025-11-30 11:15:20 +0500

    created using VS Code
    while practicing the code in the lecture of Langsmith_masterclass_Practice_Code

--- Langsmith_masterclass_Practice_Code/3_rag_v3.py
+++ Langsmith_masterclass_Practice_Code/3_rag_v3.py
@@ -49,3 +49,45 @@ def setup_pipeline(pdf_path: str, chunk_size=1000, chunk_overlap=150):
     vs = build_vectorstore(splits)
     
     return vs
+
+# -------------- model, prompt, and run -----------------
+
+llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
+
+prompt = ChatPromptTemplate.from_template([
+    ('system', "Answer only from the provided context. if not found, say you don't know."),
+    ('human', "Question: {question}\n\nContext:\n{context}")
+
+
+])
+
+def format_docs(docs):
+    return "\n\n".join([d.page_content for d in docs])
+
+# -----------------------one top-level (root) run -------------------------
+@traceable(name='pdf_rag_full_run')
+def setup_pipeline_and_query(pdf_path: str, question: str):
+
+    # parent setup run (child of root)
+    vectorstore = setup_pipeline(pdf_path, chunk_size=1000, chunk_overlap=150)
+
+    # retrieve relevant docs
+    retriever = vectorstore.as_retriever(search_type='similarity', search_kwargs={'k': 4})
+
+    parallel = RunnableParallel({
+        'context': retriever | RunnableLambda(format_docs),
+        'question': RunnablePassthrough(),
+    })
+
+    chain = parallel | prompt | llm | StrOutputParser()
+
+    # This langChain run stays under the same root (since we are inside this traced function)
+    lc_config = {"run_name": "pdf_rag_query"}
+    return  chain.invoke(question=question, config=lc_config)
+
+# ----------------------- CLI -------------------------
+if __name__ == "__main__":
+    print("PDF RAG ready. Ask a question (or ctrl+c to exit).")
+    q = input("\nQ: ").strip()
+    ans = setup_pipeline_and_query(PDF_PATH, q)
+    print(f"\nA: {ans}\n")
