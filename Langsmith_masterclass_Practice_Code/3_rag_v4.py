b9e1ab340eaf4c0b607444d8976ab29c9d66c8da
Saif Ullah <muhammadalisaif785@gmail.com>
2025-12-01 17:56:30 +0500

    created using VS Code
    while practicing the code in the lecture of Langsmith_masterclass_Practice_Code

---
+++ Langsmith_masterclass_Practice_Code/3_rag_v4.py
@@ -0,0 +1,26 @@
+# pip install -U langchain langchain-openai langchain-community faiss-cpu pypdf python-dotenv langsmith
+
+import os
+import json
+import hashlib
+from pathlib import Path
+from dotenv import load_dotenv
+
+from langsmith import traceable
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+PDF_PATH = 'islr.pdf'  # change to your file
+INDEX_ROOT = Path('./indexes')
+INDEX_ROOT.mkdir(exist_ok=True)
+
+
+# -------------- helpers (traced ) -----------------
