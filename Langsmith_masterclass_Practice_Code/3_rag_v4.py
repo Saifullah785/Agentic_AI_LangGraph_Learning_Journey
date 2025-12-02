62f5135098e7a7029d6854e16859d02acd2e94f3
Saif Ullah <muhammadalisaif785@gmail.com>
2025-12-02 18:33:08 +0500

    created using vs code
    While practicing the code in the lecture of Langsmith_masterclass_Practice_Code

--- Langsmith_masterclass_Practice_Code/3_rag_v4.py
+++ Langsmith_masterclass_Practice_Code/3_rag_v4.py
@@ -24,3 +24,27 @@ INDEX_ROOT.mkdir(exist_ok=True)
 
 
 # -------------- helpers (traced ) -----------------
+
+@traceable(name="load_pdf")
+
+def load_pdf(path: str):
+
+    return PyPDFLoader(path).load()
+
+@traceable(name="split_documents")
+
+def split_documents(docs, chunk_size=1000, chunk_overlap=150):
+
+    splitter = RecursiveCharacterTextSplitter(
+
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
+    return splitter.split_documents(docs)
+
+@traceable(name='build_vectorstore')
+
+def build_vectorstore(split, embed_model_name, index_path: str):
+
+    emb = OpenAIEmbeddings(model=embed_model_name)
+
+    return FAISS.from_documents(split, emb)
