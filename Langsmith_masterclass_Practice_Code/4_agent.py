63bb62475cbf87031df6373716789c7937f2c78c
Saif Ullah <muhammadalisaif785@gmail.com>
2025-12-09 16:59:45 +0500

    created using vs code
    while practicing the code in the class of Langsmith_masterclass_Practice_Code

--- Langsmith_masterclass_Practice_Code/4_agent.py
+++ Langsmith_masterclass_Practice_Code/4_agent.py
@@ -21,3 +21,37 @@ def get_weather_data(city: str) -> str:
     response = requests.get(url)
     
     return response.json()
+
+
+llm = ChatOpenAI()
+
+# step 2: Pull the React prompt from LangChain Hub
+
+prompt = hub.pull("hwchase17/react") # pull the standard ReAct prompt
+
+
+# step 3: Create the ReAct agent manually with the pulled prompt
+agent = create_react_agent(
+    llm=llm,
+    tools=[search_tool, get_weather_data],
+    prompt=prompt
+)
+# Step 4: Wrap it with AgentExecutor
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[search_tool, get_weather_data],
+    verbose=True
+    max_iterations=5
+)
+
+
+
+# what is the release date of Dhadak 2?
+# what is the current temp of gurgaon
+# Identify the birthplace city of kalpana chawla (search) and give its current temperature.
+
+# Step 5: Invoke 
+response = agent_executor.invoke({"input": "What is the current temp of gurgaon"})
+print(response)
+
+print(response['output'])
